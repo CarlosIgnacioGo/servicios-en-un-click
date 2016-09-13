e9320bf23d7896e2bd0eeb62d184929a06853941
Carlos Gomez <carlos221519@gmail.com>
2016-09-12 22:03:08 -0300

    Autocompletado

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,4 @@
-Comment.destroy_all
-Service.destroy_all
 
-25.times do
-	Service.create(title: Faker::Commerce.department(5), description: Faker::Lorem.paragraphs(3), address: Faker::Address.street_address, email: Faker::Internet.email,phone: Faker::PhoneNumber.cell_phone)
-end
+AdminUser.destroy_all
 
 AdminUser.create!(email: 'admin@seuc.com', password: 'adminluis', password_confirmation: 'password')
